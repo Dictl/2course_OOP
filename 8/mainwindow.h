a66f33ef0509f478902c01576f496cfc1bf1d2f4
Yana <144812679+Dictl@users.noreply.github.com>
2024-12-22 20:09:00 +0300

    final

--- 8/mainwindow.h
+++ 8/mainwindow.h
@@ -31,9 +31,9 @@ protected:
         if (s==0) object = new Sine<TComplex>(deg*2);
         else object = new SineIntegral<TComplex>(deg*2);
         TComplex ar;
-        painter.setPen(Qt::blue);
+        painter.setPen(Qt::red);
         double x1=f, y1, x2=f, y2;
-        std::stringstream ss_;
+        //std::stringstream ss_;
         ar = TComplex(0);
         y1 = (object->solve_for_x(ar)).get_real();
         double scale = 800 / (b - f);
@@ -41,7 +41,8 @@ protected:
             x2+=0.1;
             ar = TComplex(x2);
             y2 = (object->solve_for_x(ar)).get_real();
-            painter.drawPoint(((x1-f)*scale),(y1*scale+400));
+            //painter.drawPoint(((x1-f)*scale),(y1*scale+400));
+            painter.drawLine(((x1-f)*scale),((-y1)*scale+400),((x2-f)*scale),((-y2)*scale+400));
             x1=x2;
             y1=y2;
         }
